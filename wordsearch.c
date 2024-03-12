9e8bf78d248269c30901195ff9d54afd24044bfe
Christine Doan <angelstudyblr@gmail.com>
2024-03-12 00:24:54 -0700

    shit no no work

--- wordsearch.c
+++ wordsearch.c
@@ -4,9 +4,14 @@
 
 // Declarations of the two functions you will implement
 // Feel free to declare any helper functions or global variables
+int bSize;
+
+// Declaring functions:
+int check_surrounding_indexes(char **arr, char *word, int index, int row, int column, int size);
+void upperCase(char* word);
 void printPuzzle(char** arr);
 void searchPuzzle(char** arr, char* word);
-int bSize;
+void printSolution(int** arr);
 
 // Main function, DO NOT MODIFY 	
 int main(int argc, char **argv) {
@@ -80,7 +85,7 @@ void printPuzzle(char** arr) {
     printf("\n");
 }
 
-void printtest(int** arr) {
+void printSolution(int** arr) {
 	// This function will print out the complete puzzle grid (arr). 
     for (int i = 0; i < bSize; i++) {
         for (int j = 0; j < bSize; j++) {
@@ -89,6 +94,41 @@ void printtest(int** arr) {
         printf("\n");
     }
     printf("\n");
+}
+
+int check_surrounding_indexes(char **arr, char *word, int index, int row, int column, int size) {
+    int **currentGrid = (int**) malloc(bSize*sizeof(int*));
+
+    if (index == size - 1) {  // If the index reaches the end of the word, return 1 and exit
+        return 1;
+    }
+
+    index++;
+
+    if (*(*(arr + row) + column) != *(word + index)) { // If the current character doesn't match, exit
+        return 0;
+    }
+
+    for (int i = row - 1; i <= row + 1; i++) {
+        for (int j = column - 1; j <= column + 1; j++) {
+            if (i >= 0 && j >= 0) {
+                if (i < bSize && j < bSize) {
+                    if (!(i == row && j == column)) {
+                        if (*(*(arr + i) + j) == *(word + index)) {
+                            if (check_surrounding_indexes(arr, word, index, i, j, size) == 1) {
+                            *(*(currentGrid + i) + j) = *(*(arr + i) + j) * 10 + (index + 1);
+                            return 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    return 0;
 }
 
 void searchPuzzle(char** arr, char* word) {
@@ -112,16 +152,19 @@ void searchPuzzle(char** arr, char* word) {
     */
 
    upperCase(word); // Turn word into uppercase
+
    int i = 0, j = 0; // i = row, j = column
    int **foundGrid = (int**) malloc(bSize*sizeof(int*)); // The result 2D array 
    int wordLength = strlen(word);
-   int found = 0;
+   int found = 0, index = 0;
 
    // Traversing the grid
    // Check if a char in the grid is also in the word.
    // 1. If char at arr[i][j] matches char in word, arr[i][j] = word[matching char pos]
    // 2. Check surrounding chars within 1 index
    // 3. Else, arr[i][j] = 0
+
+   // Turning resulting grid into a 2D array of 0s
    
    for (i = 0; i < bSize; i++) {
     *(foundGrid + i) = (int*)malloc(bSize*sizeof(int));
@@ -130,25 +173,27 @@ void searchPuzzle(char** arr, char* word) {
     }
    }
 
-   
-
-//    for (i = 0; i < bSize; i++) {
-//     for (j = 0; j < bSize; j++) {
-//         char* current = *(arr+i)+j;
-//         for (int k = 0; k < wordLength; k++) {
-//             if (*current == *word) {
-//                 *(*(foundGrid + i) + j) = 1;
-//             }
-//         }
-//     }
-//    }
-
-   printtest(foundGrid);
+   for (i = 0; i < bSize; i++) {
+    for (j = 0; j < bSize; j++) {
+        if (*(*(arr + i) + j) == *(word + index)) {
+            if (check_surrounding_indexes(arr, word, index, i, j, wordLength)) {
+                if (*(*(foundGrid + i) + j) > 0) {
+                    *(*(foundGrid + i) + j) = *(*(foundGrid + i) + j) * 10 + (index + 1);
+                } else {
+                    *(*(foundGrid + i) + j) = 1;
+                }
+                found = 1;
+            }
+        }
+    }
+   }
    
 
    if (found) {
     printf("Word found!\n");
+    printSolution(foundGrid);
    } else {
     printf("Word not found!\n");
    }
 }
+
